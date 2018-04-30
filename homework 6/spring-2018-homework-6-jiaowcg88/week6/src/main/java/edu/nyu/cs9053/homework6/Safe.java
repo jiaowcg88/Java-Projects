60f0db1209124d6c8a6fca219d15799d4d037a64
Jiao (Joanna) Wu <jw4321@nyu.edu>
2018-04-30 16:56:59 -0400

    Add files via upload

---
+++ homework 6/spring-2018-homework-6-jiaowcg88/week6/src/main/java/edu/nyu/cs9053/homework6/Safe.java
@@ -0,0 +1,21 @@
+package edu.nyu.cs9053.homework6;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+/**
+ * User: blangel
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Safe {
+
+    static final long INVALID = 0L;
+
+    /**
+     * @return the deposit-safe's password
+     */
+    long password() default INVALID;
+
+}
